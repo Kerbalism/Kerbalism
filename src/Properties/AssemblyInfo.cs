10bd149bd3d9b235deae1a789fa82159c1f08f6f
ShotgunNinja <ShotgunNinja@users.noreply.github.com>
2016-06-08 05:11:47 +0200

    v1.0.2

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.0.1.0")]
-[assembly: AssemblyFileVersion ("1.0.1.0")]
+[assembly: AssemblyVersion ("1.0.2.0")]
+[assembly: AssemblyFileVersion ("1.0.2.0")]
