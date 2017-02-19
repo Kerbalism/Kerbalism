a0875e7daed68c889b30bec69b4dd5fd4d2537a0
ShotgunNinja <ShotgunNinja@users.noreply.github.com>
2017-02-19 11:04:18 +0100

    new utility to parse 'dump' specs in process

---
+++ src/Utility/DumpSpec.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace KERBALISM {
+
+
+public sealed class DumpSpecs
+{
+  public DumpSpecs(string value)
+  {
+    // if empty or false: don't dump anything
+    if (value.Length == 0 || value.ToLower() == "false")
+    {
+      any = false;
+      list = new List<string>();
+    }
+    // if true: dump everything
+    else if (value.ToLower() == "true")
+    {
+      any = true;
+      list = new List<string>();
+    }
+    // all other cases: dump only specified resources
+    else
+    {
+      any = false;
+      list = Lib.Tokenize(value, ',');
+    }
+  }
+
+  // return true if the resource should dump
+  public bool check(string res_name)
+  {
+    return any || list.Contains(res_name);
+  }
+
+  bool         any;
+  List<string> list;
+}
+
+
+} // KERBALISM
+
+
+
