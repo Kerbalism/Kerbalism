3b1b48a411a90c72eb955c03b894b3dc43158a7c
ShotgunNinja <ShotgunNinja@users.noreply.github.com>
2017-05-16 05:26:00 +0200

    Harvester module changes
    - improved UI: now show reason resource can't be extracted (if any)
    - fixed drill head -> terrain collision detection
    - now can specify 'length' in config node: that being the tolerable distance from the drill head transform and the ground

--- src/Modules/Harvester.cs
+++ src/Modules/Harvester.cs
@@ -16,17 +16,19 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
   [KSPField] public double min_pressure = 0.0;              // minimal pressure required, in kPA
   [KSPField] public double rate = 0.0;                      // rate of resource to extract at 100% abundance
   [KSPField] public double ec_rate = 0.0;                   // rate of ec consumption per-second, irregardless of abundance
-  [KSPField] public string drill = string.Empty;            // the drill transform
-
+  [KSPField] public string drill = string.Empty;            // the drill head transform
+  [KSPField] public double length = 5.0;                    // tolerable distance between drill head and the ground (length of the extendible part)
 
   // persistence
   [KSPField(isPersistant = true)] public bool deployed;     // true if the harvester is deployed
   [KSPField(isPersistant = true)] public bool running;      // true if the harvester is running
-  [KSPField(isPersistant = true)] public bool starved;      // true if the resource can't be harvested
-
+  [KSPField(isPersistant = true)] public string issue = string.Empty; // if not empty, the reason why resource can't be harvested
 
   // show abundance level
   [KSPField(guiActive = false, guiName = "_")] public string Abundance;
+
+  // the drill head transform
+  Transform drill_head;
 
 
   public override void OnStart(StartState state)
@@ -36,62 +38,46 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
 
     // assume deployed if there is no animator
     deployed |= part.FindModuleImplementing<ModuleAnimationGroup>() == null;
+
+    // setup ui
+    Fields["Abundance"].guiName = Lib.BuildString(resource, " abundance");
+
+    // get drill head transform only once
+    if (drill.Length > 0) drill_head = part.FindModelTransform(drill);
   }
 
 
   public void Update()
   {
-    // in editor and flight, update ui button label
-    Events["Toggle"].guiName = Lib.StatusToggle(title, running ? "running" : "stopped");
+    // in editor, merely update ui button label
+    if (Lib.IsEditor())
+    {
+      Events["Toggle"].guiName = Lib.StatusToggle(title, running ? "running" : "stopped");
+    }
 
     // if in flight, and the stock planet resource system is online
     if (Lib.IsFlight() && ResourceMap.Instance != null)
     {
-      // shortcut
-      CelestialBody body = vessel.mainBody;
+      // sample abundance
+      double abundance = SampleAbundance();
 
-      // determine if overall situation is valid for extraction
-      bool valid = false;
-      if (deployed)
-      {
-        switch(type)
-        {
-          case 0: valid = vessel.Landed && GroundContact(); break;
-          case 1: valid = body.ocean && (vessel.Splashed || vessel.altitude < 0.0); break;
-          case 2: valid = body.atmosphere && vessel.altitude < body.atmosphereDepth; break;
-          case 3: valid = vessel.altitude > body.atmosphereDepth || !body.atmosphere; break;
-        }
-      }
-
-      // if situation is valid
-      double abundance = 0.0;
-      if (valid)
-      {
-        // get abundance
-        AbundanceRequest request = new AbundanceRequest
-        {
-          ResourceType = (HarvestTypes)type,
-          ResourceName = resource,
-          BodyId = body.flightGlobalsIndex,
-          Latitude = vessel.latitude,
-          Longitude = vessel.longitude,
-          Altitude = vessel.altitude,
-          CheckForLock = false
-        };
-        abundance = ResourceMap.Instance.GetAbundance(request);
-      }
-
-      // determine if resources can be harvested
-      // - invalid conditions
-      // - abundance below threshold
-      // - pressure below threshold
-      starved = !valid || abundance <= min_abundance || (type == 2 && body.GetPressure(vessel.altitude) <= min_pressure);
+      // determine if resource can be extracted
+      issue = DetectIssue(abundance);
 
       // update ui
-      Events["Toggle"].guiActive = valid;
-      Fields["Abundance"].guiActive = valid;
-      Fields["Abundance"].guiName = Lib.BuildString(resource, " abundance");
-      Abundance = abundance > double.Epsilon ? Lib.HumanReadablePerc(abundance, "F2") : "none";
+      Events["Toggle"].guiActive = deployed;
+      Fields["Abundance"].guiActive = deployed;
+      if (deployed)
+      {
+        string status = !running
+          ? "stopped"
+          : issue.Length == 0
+          ? "running"
+          : Lib.BuildString("<color=yellow>", issue, "</color>");
+
+        Events["Toggle"].guiName = Lib.StatusToggle(title, status);
+        Abundance = abundance > double.Epsilon ? Lib.HumanReadablePerc(abundance, "F2") : "none";
+      }
     }
   }
 
@@ -100,7 +86,7 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
   {
     if (Lib.IsEditor()) return;
 
-    if (deployed && running && !starved)
+    if (deployed && running && issue.Length == 0)
     {
       resource_recipe recipe = new resource_recipe();
       recipe.Input("ElectricCharge", ec_rate * Kerbalism.elapsed_s);
@@ -112,7 +98,7 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
 
   public static void BackgroundUpdate(Vessel v, ProtoPartModuleSnapshot m, Harvester harvester, double elapsed_s)
   {
-    if (Lib.Proto.GetBool(m, "deployed") && Lib.Proto.GetBool(m, "running") && !Lib.Proto.GetBool(m, "starved"))
+    if (Lib.Proto.GetBool(m, "deployed") && Lib.Proto.GetBool(m, "running") && Lib.Proto.GetString(m, "issue").Length == 0)
     {
       resource_recipe recipe = new resource_recipe();
       recipe.Input("ElectricCharge", harvester.ec_rate * elapsed_s);
@@ -128,29 +114,82 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
     running = !running;
   }
 
-
-  bool GroundContact()
+  // return resource abundance at vessel position
+  double SampleAbundance()
   {
-    // hard-coded margin
-    const double margin = 0.2;
-
-    // if there is no drill transform specified, assume ground contact
-    if (drill.Length == 0) return true;
-
-    // get terrain height below vessel center
-    double terrain_height = Lib.TerrainHeight(vessel);
-
-    // get drill transform position in world space
-    Transform drill_transform = part.FindModelComponent<Transform>(drill);
-    if (drill_transform == null) return true;
-
-    // calculate drill distance from zero altitude surface
-    double drill_altitude = (drill_transform.position - vessel.mainBody.position).magnitude - vessel.mainBody.Radius;
-
-    // we have ground contact if the drill altitude is less than terrain height
-    return terrain_height + margin > drill_altitude;
+    // get abundance
+    AbundanceRequest request = new AbundanceRequest
+    {
+      ResourceType = (HarvestTypes)type,
+      ResourceName = resource,
+      BodyId = vessel.mainBody.flightGlobalsIndex,
+      Latitude = vessel.latitude,
+      Longitude = vessel.longitude,
+      Altitude = vessel.altitude,
+      CheckForLock = false
+    };
+    return ResourceMap.Instance.GetAbundance(request);
   }
 
+
+  // return the reason why resource can't be harvested, or an empty string otherwise
+  string DetectIssue(double abundance)
+  {
+    // shortcut
+    CelestialBody body = vessel.mainBody;
+
+    // check situation
+    switch(type)
+    {
+      case 0:
+        bool land_valid = vessel.Landed && GroundContact();
+        if (!land_valid) return "no ground contact";
+          break;
+
+      case 1:
+        bool ocean_valid = body.ocean && (vessel.Splashed || vessel.altitude < 0.0);
+        if (!ocean_valid) return "not in ocean";
+        break;
+
+      case 2:
+        bool atmo_valid = body.atmosphere && vessel.altitude < body.atmosphereDepth;
+        if (!atmo_valid) return "not in atmosphere";
+        break;
+
+      case 3:
+        bool space_valid = vessel.altitude > body.atmosphereDepth || !body.atmosphere;
+        if (!space_valid) return "not in space";
+        break;
+    }
+
+    // check against pressure
+    if (type == 2 && body.GetPressure(vessel.altitude) <= min_pressure)
+    {
+      return "pressure below threshold";
+    }
+
+    // check against abundance
+    if (abundance <= min_abundance)
+    {
+      return "abundance below threshold";
+    }
+
+    // all good
+    return string.Empty;
+  }
+
+
+  // return true if the drill head penetrate the ground
+  bool GroundContact()
+  {
+    // if there is no drill transform specified, or if the specified one doesn't exist, assume ground contact
+    if (drill_head == null) return true;
+
+    // get distance from drill head to terrain
+    // - the drill head transform of stock parts doesn't refer to the drill head (of course),
+    //   but to the start of the extendible portion of the drill
+    return Lib.TerrainHeight(vessel.mainBody, drill_head.position) < length;
+  }
 
   // action groups
   [KSPAction("Start/Stop Harvester")] public void Action(KSPActionParam param) { Toggle(); }
@@ -190,7 +229,7 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
   // animation group support
   public void EnableModule()      { deployed = true; }
   public void DisableModule()     { deployed = false; running = false; }
-  public bool ModuleIsActive()    { return running && !starved; }
+  public bool ModuleIsActive()    { return running && issue.Length == 0; }
   public bool IsSituationValid()  { return true; }
 
   // module info support
@@ -204,9 +243,4 @@ public sealed class Harvester : PartModule, IAnimatedModule, IModuleInfo, ISpeci
 }
 
 
-
 } // KERBALISM
-
-
-
-
