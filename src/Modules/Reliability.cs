51cbdfe029a09c587c12e232802662bb2c9eceed
schrema <manull@gmx.de>
2017-09-25 15:20:15 +0200

    Update Reliability.cs

--- src/Modules/Reliability.cs
+++ src/Modules/Reliability.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using RealFuels;
 
 
 namespace KERBALISM {
