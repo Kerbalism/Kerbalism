d7f1a89fa3b7ed7ab47aaa0cd2226f9a0c44edbd
N70 <gameexpertnetwork@googlemail.com>
2018-04-06 15:54:56 -0400

    Revert to pre-Kopernicus version

--- buildscripts/Jenkins/avc_to_assembly.sh
+++ buildscripts/Jenkins/avc_to_assembly.sh
@@ -2,7 +2,9 @@
 MAJOR=$(cat $WORKSPACE/GameData/$JOB_NAME/$JOB_NAME.version | jq '.VERSION.MAJOR')
 MINOR=$(cat $WORKSPACE/GameData/$JOB_NAME/$JOB_NAME.version | jq '.VERSION.MINOR')
 PATCH=$(cat $WORKSPACE/GameData/$JOB_NAME/$JOB_NAME.version | jq '.VERSION.PATCH')
-BUILD=$(cat $WORKSPACE/GameData/$JOB_NAME/$JOB_NAME.version | jq '.VERSION.BUILD')
-export MOD_VERSION=$MAJOR.$MINOR.$PATCH.$BUILD
+jq --argjson build $BUILD_NUMBER '.VERSION.BUILD=[$build|tonumber]' $WORKSPACE/GameData/Kerbalism/Kerbalism.version > version.tmp
+rm $WORKSPACE/GameData/Kerbalism/Kerbalism.version
+mv version.tmp $WORKSPACE/GameData/Kerbalism/Kerbalism.version
+export MOD_VERSION=$MAJOR.$MINOR.$PATCH.$BUILD_NUMBER
 echo "Mod version: $MOD_VERSION"
 sed -i -E 's/Assembly(\w*)Version\("[0-9\*\.]+"\)/Assembly\1Version("'"$MOD_VERSION"'")/' $WORKSPACE/src/Properties/AssemblyInfo.cs
