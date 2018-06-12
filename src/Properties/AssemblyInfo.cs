7c53f97d9dfe44653a9100692c475abbb01a3ca9
steamport <steamport@protonmail.com>
2018-06-11 23:05:49 -0400

    Revert "ClickThroughBlocker (#55)"
    This reverts commit 880141de36d24c43ce8db537357526ad642b8d25.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -26,5 +26,3 @@ using System.Runtime.InteropServices;
 // Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("1.6.1.0")]
 [assembly: AssemblyFileVersion("1.6.1.0")]
-
-[assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
--- src/UI/UI.cs
+++ src/UI/UI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace KERBALISM
 {
 
