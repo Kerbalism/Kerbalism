a5b8415f31beb15c7bd74682849c74e64c4432a0
Martin Domig <MartinDomig@users.noreply.github.com>
2018-07-04 14:37:58 +0200

    Support builds on Mac (#108)
    * Fixed broken low_battery localization.
    
    Also included a log message entry if any localization texts are missing.
    
    * Added a section on how to get going with MacOS
    
    Little guide on how to build on MacOS
    
    * Updated ToC
    
    * Setup your KSP folder for VisualStudio
    
    Script to make KSP compatible with the VisualStudio project

---
+++ buildscripts/setup_mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script will setup your KSP installation folder to work with the Kerbalism visual studio project.
+
+# Modify this variable to point to the installation directory of the KSP folder you want to use
+# for development. This is the default location if you used the installer.
+MY_KSP_DIR=/Applications/KSP_osx
+
+echo "export KSPDEVDIR=$MY_KSP_DIR" >> ~/.bash_profile
+. ~/.bash_profile
+
+cd $KSPDEVDIR
+mkdir -p KSP_x64_Data/Managed
+cd KSP_x64_Data/Managed
+
+ln -s ../../Launcher.app/Contents/Resources/Data/Managed/UnityEngine.dll .
+ln -s ../../Launcher.app/Contents/Resources/Data/Managed/UnityEngine.UI.dll .
+ln -s ../../KSP.app/Contents/Resources/Data/Managed/* . 2>/dev/null
+
+cd $KSPDEVDIR
