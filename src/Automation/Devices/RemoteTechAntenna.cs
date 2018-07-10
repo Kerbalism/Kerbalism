98d172ac6ce3da0c5d1b2b0ef85f5b924926b546
Martin Domig <martin.domig@finkzeit.at>
2018-07-10 17:28:35 +0200

    Vessels without power will no longer work as relays in RemoteTech.
    This is done by forcing a blackout on a vessel without power, the same
    mechanism that is used for CME storms.

--- src/Automation/Devices/RemoteTechAntenna.cs
+++ src/Automation/Devices/RemoteTechAntenna.cs
@@ -16,7 +16,7 @@ namespace KERBALISM
 
 		public override string Name()
 		{
-			return "RT antenna";
+			return "antenna";
 		}
 
 		public override uint Part()
@@ -56,7 +56,7 @@ namespace KERBALISM
 
 		public override string Name()
 		{
-			return "RT antenna";
+			return "antenna";
 		}
 
 		public override uint Part()
