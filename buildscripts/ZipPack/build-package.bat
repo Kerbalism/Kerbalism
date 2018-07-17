352ecbd97eecf9694080453085a1251f3325456a
PiezPiedPy <githubpie@maxautodesign.com>
2018-07-17 18:26:49 +0100

    VS Configurations etc for KSP1.3.1 BackPort (#145)
    * Build: Add VS configs for KSP1.3.1
    
    Added configs `Debug 1.3` and `Release 1.3` that have the KSP13 define and set output dir with the KSPBACKPORTDIR Env var.
    Also added PostEvents for them to build the correct Zip.
    
    * Build: Updated the zip build .bat file to use the correct shaders
    
    * CONTRIBUTING.md: Added BackPort information

--- buildscripts/ZipPack/build-package.bat
+++ buildscripts/ZipPack/build-package.bat
@@ -8,6 +8,9 @@ rem get parameters that are passed by visual studio post build event
 SET TargetName=%1
 SET Dllversion=%~n2
 SET KSPversion=%3
+SET Shaders=%~n3
+SET Shaders=%Shaders:*KSP=%
+
 
 rem make sure the initial working directory is the one containing the current script
 SET scriptPath=%~dp0
@@ -31,10 +34,15 @@ xcopy /y /e "..\..\..\GameData\%TargetName%\*" .
 xcopy /y ..\..\..\CHANGELOG.md .
 xcopy /y ..\..\..\License .
 
+IF EXIST Shaders\ rd /s /q Shaders
+mkdir Shaders
+cd Shaders
+xcopy /y /e "..\..\..\..\buildscripts\Shaders\%Shaders%\*" .
+
 echo.
 echo Compressing %TargetName% for %KSPversion% Release Package...
 IF EXIST "%rootPath%%TargetName%*_For_%KSPversion%.zip" del "%rootPath%%TargetName%*_For_%KSPversion%.zip"
-"%scriptPath%7za.exe" a "..\..\..\%TargetName%%Dllversion%_For_%KSPversion%.zip" ..\..\GameData
+"%scriptPath%7za.exe" a "..\..\..\..\%TargetName%%Dllversion%_For_%KSPversion%.zip" ..\..\..\GameData
 
 cd "%rootPath%"
 rd /s /q package
