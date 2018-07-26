faa0db8aab0f8e073ea2fede74d97e06ba523af5
PiezPiedPy <githubpie@maxautodesign.com>
2018-07-26 20:51:51 +0100

    Signals: Framework for RemoteTech

--- src/Comms/ConnectionInfo.cs
+++ src/Comms/ConnectionInfo.cs
@@ -76,54 +76,16 @@ namespace KERBALISM
 				DB.Vessel(v).hyspos_signal = 0.0;
 			}
 
-			List<ModuleDataTransmitter> transmitters;
-
-			// if vessel is loaded
-			if (v.loaded)
+			// CommNet or simple signal system
+			if (!RemoteTech.Enabled)
 			{
-				// find transmitters
-				transmitters = v.FindPartModulesImplementing<ModuleDataTransmitter>();
+				List<ModuleDataTransmitter> transmitters;
 
-				if (transmitters != null)
+				// if vessel is loaded
+				if (v.loaded)
 				{
-					foreach (ModuleDataTransmitter t in transmitters)
-					{
-						if (t.antennaType == AntennaType.INTERNAL) // do not include internal data rate
-							internal_cost += t.DataResourceCost * t.DataRate;
-						else
-						{
-							// do we have an animation
-							ModuleDeployableAntenna animation = t.part.FindModuleImplementing<ModuleDeployableAntenna>();
-							if (animation != null)
-							{
-								// only include data rate if transmitter is extended
-								if (animation.deployState == ModuleDeployablePart.DeployState.EXTENDED)
-								{
-									rate += t.DataRate;
-									external_cost += t.DataResourceCost * t.DataRate;
-								}
-							}
-							// no animation
-							else
-							{
-								rate += t.DataRate;
-								external_cost += t.DataResourceCost * t.DataRate;
-							}
-						}
-					}
-				}
-			}
-
-			// if vessel is not loaded
-			else
-			{
-				// find proto transmitters
-				foreach (ProtoPartSnapshot p in v.protoVessel.protoPartSnapshots)
-				{
-					// get part prefab (required for module properties)
-					Part part_prefab = PartLoader.getPartInfoByName(p.partName).partPrefab;
-
-					transmitters = part_prefab.FindModulesImplementing<ModuleDataTransmitter>();
+					// find transmitters
+					transmitters = v.FindPartModulesImplementing<ModuleDataTransmitter>();
 
 					if (transmitters != null)
 					{
@@ -134,12 +96,11 @@ namespace KERBALISM
 							else
 							{
 								// do we have an animation
-								ProtoPartModuleSnapshot m = p.FindModule("ModuleDeployableAntenna");
-								if (m != null)
+								ModuleDeployableAntenna animation = t.part.FindModuleImplementing<ModuleDeployableAntenna>();
+								if (animation != null)
 								{
 									// only include data rate if transmitter is extended
-									string deployState = Lib.Proto.GetString(m, "deployState");
-									if (deployState == "EXTENDED")
+									if (animation.deployState == ModuleDeployablePart.DeployState.EXTENDED)
 									{
 										rate += t.DataRate;
 										external_cost += t.DataResourceCost * t.DataRate;
@@ -155,47 +116,95 @@ namespace KERBALISM
 						}
 					}
 				}
-			}
 
-			// if CommNet is enabled
-			if (HighLogic.fetch.currentGame.Parameters.Difficulty.EnableCommNet)
-			{
-				// are we connected to DSN
-				if (v.connection != null)
+				// if vessel is not loaded
+				else
 				{
-					if (v.connection.IsConnected)
+					// find proto transmitters
+					foreach (ProtoPartSnapshot p in v.protoVessel.protoPartSnapshots)
 					{
-						linked = true;
-						status = v.connection.ControlPath.First.hopType == CommNet.HopType.Home ? LinkStatus.direct_link : LinkStatus.indirect_link;
-						strength = v.connection.SignalStrength;
-						rate = rate * strength;
-						target_name = Lib.Ellipsis(Localizer.Format(v.connection.ControlPath.First.end.displayName).Replace("Kerbin", "DSN"), 20);
-						return;
-					}
+						// get part prefab (required for module properties)
+						Part part_prefab = PartLoader.getPartInfoByName(p.partName).partPrefab;
 
-					// is loss of connection due to plasma blackout
-					else if (Lib.ReflectionValue<bool>(v.connection, "inPlasma"))  // calling InPlasma causes a StackOverflow :(
-					{
-						status = LinkStatus.plasma;
-						rate = 0.0;
-						internal_cost = 0.0;
-						external_cost = 0.0;
-						return;
+						transmitters = part_prefab.FindModulesImplementing<ModuleDataTransmitter>();
+
+						if (transmitters != null)
+						{
+							foreach (ModuleDataTransmitter t in transmitters)
+							{
+								if (t.antennaType == AntennaType.INTERNAL) // do not include internal data rate
+									internal_cost += t.DataResourceCost * t.DataRate;
+								else
+								{
+									// do we have an animation
+									ProtoPartModuleSnapshot m = p.FindModule("ModuleDeployableAntenna");
+									if (m != null)
+									{
+										// only include data rate if transmitter is extended
+										string deployState = Lib.Proto.GetString(m, "deployState");
+										if (deployState == "EXTENDED")
+										{
+											rate += t.DataRate;
+											external_cost += t.DataResourceCost * t.DataRate;
+										}
+									}
+									// no animation
+									else
+									{
+										rate += t.DataRate;
+										external_cost += t.DataResourceCost * t.DataRate;
+									}
+								}
+							}
+						}
 					}
 				}
 
-				// no connection
-				rate = 0.0;
-				internal_cost = 0.0;
-				external_cost = 0.0;
-				return;
+				// if CommNet is enabled
+				if (HighLogic.fetch.currentGame.Parameters.Difficulty.EnableCommNet)
+				{
+					// are we connected to DSN
+					if (v.connection != null)
+					{
+						if (v.connection.IsConnected)
+						{
+							linked = true;
+							status = v.connection.ControlPath.First.hopType == CommNet.HopType.Home ? LinkStatus.direct_link : LinkStatus.indirect_link;
+							strength = v.connection.SignalStrength;
+							rate = rate * strength;
+							target_name = Lib.Ellipsis(Localizer.Format(v.connection.ControlPath.First.end.displayName).Replace("Kerbin", "DSN"), 20);
+							return;
+						}
+
+						// is loss of connection due to plasma blackout
+						else if (Lib.ReflectionValue<bool>(v.connection, "inPlasma"))  // calling InPlasma causes a StackOverflow :(
+						{
+							status = LinkStatus.plasma;
+							rate = 0.0;
+							internal_cost = 0.0;
+							external_cost = 0.0;
+							return;
+						}
+					}
+
+					// no connection
+					rate = 0.0;
+					internal_cost = 0.0;
+					external_cost = 0.0;
+					return;
+				}
+
+				// the simple stupid always connected signal system
+				linked = true;
+				status = LinkStatus.direct_link;
+				strength = 1;    // 100 %
+				target_name = "DSN: KSC";
 			}
 
-			// the simple stupid always connected signal system
-			linked = true;
-			status = LinkStatus.direct_link;
-			strength = 1;    // 100 %
-			target_name = "DSN: KSC";
+			// RemoteTech signal system
+			else
+			{
+			}
 		}
 	}
 
