c75be66ff5654ac97c8e204502b1808db40de2a5
PiezPiedPy <githubpie@maxautodesign.com>
2018-07-29 19:42:45 +0100

    RemoteTech: Add summaries to methods

--- src/External/RemoteTech.cs
+++ src/External/RemoteTech.cs
@@ -3,8 +3,11 @@ using System.Reflection;
 
 namespace KERBALISM
 {
+
+	/// <summary> Contains methods for RemoteTech's API</summary>
 	public static class RemoteTech
 	{
+		// constructor
 		static RemoteTech()
 		{
 			foreach (var a in AssemblyLoader.loadedAssemblies)
@@ -27,85 +30,95 @@ namespace KERBALISM
 			}
 		}
 
-		// return true if RemoteTech is enabled for the current game
+		/// <summary> Returns true if RemoteTech is enabled for the current game</summary>
 		public static bool Enabled
 		{
 			get { return API != null && (bool)IsEnabled.Invoke(null, new Object[] { }); }
 		}
 
+		/// <summary> Enables RTCore in the Space Center scene</summary>
 		public static void EnableInSPC()
 		{
 			if (API != null && EnabledInSPC != null)
 				EnabledInSPC.Invoke(null, new Object[] { true });
 		}
 
+		/// <summary> Returns true if the vessel has a connection back to KSC</summary>
 		public static bool ConnectedToKSC(Guid id)
 		{
 			return API != null && (bool)IsConnectedKSC.Invoke(null, new Object[] { id });
 		}
 
+		/// <summary> Returns true if the vessel directly targets KSC</summary>
 		public static bool TargetsKSC(Guid id)
 		{
 			return API != null && (bool)IsTargetKSC.Invoke(null, new Object[] { id });
 		}
 
 		// return true if the vessel is connected according to RemoteTech
+		/// <summary> Returns true if the vessel has any connection</summary>
 		public static bool Connected(Guid id)
 		{
 			return API != null && (bool)IsConnected.Invoke(null, new Object[] { id });
 		}
 
+		/// <summary> Returns the signal delay of the shortest route to the KSC if any found</summary>
 		public static double GetSignalDelay(Guid id)
 		{
 			return (API != null ? (double)SignalDelay.Invoke(null, new Object[] { id }) : 0);
 		}
 
-		public static Object SetCommsBlackout(Guid id, bool flag, string origin)
+		/// <summary> Sets the comms Blackout state for the vessel</summary>
+		public static void SetCommsBlackout(Guid id, bool flag)
 		{
 			if (API != null && SetRadioBlackout != null)
-				return SetRadioBlackout.Invoke(null, new Object[] { id, flag, origin });
-			return null;
+				SetRadioBlackout.Invoke(null, new Object[] { id, flag, "Kerbalism" });
 		}
 
+		/// <summary> Gets the comms Blackout state of the vessel</summary>
 		public static bool GetCommsBlackout(Guid id)
 		{
 			return API != null && GetRadioBlackout != null && (bool)GetRadioBlackout.Invoke(null, new Object[] { id });
 		}
 
-		public static Object SetPoweredDown(Guid id, bool flag, string origin)
+		/// <summary> Sets the Powered down state for the vessel</summary>
+		public static void SetPoweredDown(Guid id, bool flag)
 		{
 			if (API != null && SetPowerDown != null)
-				return SetPowerDown.Invoke(null, new Object[] { id, flag, origin });
-			return SetCommsBlackout(id, flag, origin);  // Workaround for earlier versions of RT
+				SetPowerDown.Invoke(null, new Object[] { id, flag, "Kerbalism" });
+			else SetCommsBlackout(id, flag);  // Workaround for earlier versions of RT
 		}
 
+		/// <summary> Gets the Powered down state of the vessel</summary>
 		public static bool IsPoweredDown(Guid id)
 		{
 			return API != null && GetPowerDown != null && (bool)GetPowerDown.Invoke(null, new Object[] { id });
 		}
 
+		/// <summary> Sets the Broken state for the vessel</summary>
 		public static void SetBroken(PartModule antenna, bool broken)
 		{
 			Lib.ReflectionValue(antenna, "IsRTBroken", broken);
 		}
 
+		/// <summary> Returns true if the PartModule is a RemoteTech Antenna</summary>
 		public static bool IsAntenna(PartModule m)
 		{
 			// we test for moduleName, but could use the boolean IsRTAntenna here
 			return (m.moduleName == "ModuleRTAntenna" || m.moduleName == "ModuleRTAntennaPassive");
 		}
 
-		static Type API;
-		static MethodInfo IsEnabled;
-		static MethodInfo EnabledInSPC;
-		static MethodInfo IsConnected;
-		static MethodInfo IsConnectedKSC;
-		static MethodInfo IsTargetKSC;
-		static MethodInfo SignalDelay;
-		static MethodInfo SetRadioBlackout;
-		static MethodInfo GetRadioBlackout;
-		static MethodInfo SetPowerDown;
-		static MethodInfo GetPowerDown;
+		private static Type API;
+		private static MethodInfo IsEnabled;
+		private static MethodInfo EnabledInSPC;
+		private static MethodInfo IsConnected;
+		private static MethodInfo IsConnectedKSC;
+		private static MethodInfo IsTargetKSC;
+		private static MethodInfo SignalDelay;
+		private static MethodInfo SetRadioBlackout;
+		private static MethodInfo GetRadioBlackout;
+		private static MethodInfo SetPowerDown;
+		private static MethodInfo GetPowerDown;
 	}
 
 
