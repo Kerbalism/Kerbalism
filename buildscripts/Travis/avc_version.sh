f448e1585e968552bf64a0c337ad09027c5be264
PiezPiedPy <githubpie@maxautodesign.com>
2018-10-09 21:58:54 +0100

    Travis: avc_version.sh not change KSP_VERSION_MIN (#227)

--- buildscripts/Travis/avc_version.sh
+++ buildscripts/Travis/avc_version.sh
@@ -12,6 +12,4 @@ echo Setting $2 for KSP$1
 
 sed -i "s/\"KSP_VERSION\": {\"MAJOR\": ., \"MINOR\": ., \"PATCH\": .}/\"KSP_VERSION\": {\"MAJOR\": ${BASH_REMATCH[2]}, \"MINOR\": ${BASH_REMATCH[3]}, \"PATCH\": ${BASH_REMATCH[4]}}/g" $2
 
-sed -i "s/\"KSP_VERSION_MIN\": {\"MAJOR\": ., \"MINOR\": ., \"PATCH\": .}/\"KSP_VERSION_MIN\": {\"MAJOR\": ${BASH_REMATCH[2]}, \"MINOR\": ${BASH_REMATCH[3]}, \"PATCH\": 0}/g" $2
-
 sed -i "s/\"KSP_VERSION_MAX\": {\"MAJOR\": ., \"MINOR\": ., \"PATCH\": .}/\"KSP_VERSION_MAX\": {\"MAJOR\": ${BASH_REMATCH[2]}, \"MINOR\": ${BASH_REMATCH[3]}, \"PATCH\": ${BASH_REMATCH[4]}}/g" $2
