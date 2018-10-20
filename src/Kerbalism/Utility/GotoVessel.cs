ab17e82c98098224366e92c576ad2804973a07c7
Haully Games <34033348+HaullyGames@users.noreply.github.com>
2018-10-20 13:34:31 -0400

    Goto - Added "Set as Target" in Monitor - Save backup before every "Goto"

--- src/Kerbalism/Utility/GotoVessel.cs
+++ src/Kerbalism/Utility/GotoVessel.cs
@@ -5,17 +5,18 @@
 		[KSPField(isPersistant = true)] public static int version = 0;
 		public static void JumpToVessel(Vessel v)
 		{
+			int _idx = HighLogic.CurrentGame.flightState.protoVessels.FindLastIndex(pv => pv.vesselID == v.id);
+
+			string _saveGame = GamePersistence.SaveGame("Goto_" + version.ToString(), HighLogic.SaveFolder, SaveMode.OVERWRITE);
+			// Keep until 3 backups of Goto
+			if (version++ > 3) version = 0;
+
 			if (Lib.IsFlight())
 			{
 				FlightGlobals.SetActiveVessel(v);
 			}
 			else
 			{
-				int _idx = HighLogic.CurrentGame.flightState.protoVessels.FindLastIndex(pv => pv.vesselID == v.id);
-
-				string _saveGame = GamePersistence.SaveGame("Goto_" + version.ToString(), HighLogic.SaveFolder, SaveMode.OVERWRITE);
-				// Keep until 3 backups of Goto
-				if (version++ > 3) version = 0;
 				if (_idx != -1)
 				{
 					FlightDriver.StartAndFocusVessel(_saveGame, _idx);
@@ -25,6 +26,11 @@
 					Lib.Log("Invalid vessel Id:" + _idx);
 				}
 			}
+		}
+
+		public static void SetVesselAsTarget(Vessel v)
+		{
+			if (v != FlightGlobals.ActiveVessel) FlightGlobals.fetch.SetVesselTarget(v);
 		}
 	}
 }
