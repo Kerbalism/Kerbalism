46f1aa198abbccdd222dd5399afce7cb853fda4c
Maarten Maathuis <madman2003@shikahr.net>
2018-10-28 23:45:31 +0100

    more comments

--- src/Kerbalism/Profile/Process.cs
+++ src/Kerbalism/Profile/Process.cs
@@ -78,6 +78,8 @@ namespace KERBALISM
 		public void Execute(Vessel v, Vessel_info vi, Vessel_resources resources, double elapsed_s)
 		{
 			// evaluate modifiers
+			// if a given PartModule has a larger than 1 capacity for a process, then the multiplication happens here
+			// remember that when a process is enabled the units of process are stored in the PartModule as a pseudo-resource
 			double k = Modifiers.Evaluate(v, vi, resources, modifiers);
 
 			// only execute processes if necessary
