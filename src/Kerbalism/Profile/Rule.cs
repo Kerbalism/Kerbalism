8e586121e414d75512cdcc24b63777462a75984f
Maarten Maathuis <madman2003@shikahr.net>
2018-10-30 08:39:58 +0100

    Improve encapsulation of resource classes in preperation of non-flowable per part resource

--- src/Kerbalism/Profile/Rule.cs
+++ src/Kerbalism/Profile/Rule.cs
@@ -112,8 +112,8 @@ namespace KERBALISM
 					rd.time_since -= step * interval;
 
 					// remember if a meal is consumed/produced in this simulation step
-					res.meal_happened |= step > 0.99;
-					if (output.Length > 0) ResourceCache.Info(v, output).meal_happened |= step > 0.99;
+					if (step > 0.99) res.SetMealHappened();
+					if (output.Length > 0 && step > 0.99) ResourceCache.Info(v, output).SetMealHappened();
 				}
 
 				// if continuous, or if one or more intervals elapsed
