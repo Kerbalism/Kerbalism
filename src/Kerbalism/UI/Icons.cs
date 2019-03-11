92ce98a9fe1c4aef51ca7b895064ccfed0e84d49
steamport <steamport@protonmail.com>
2019-03-11 18:41:32 -0400

    hopefully fix the 1.3 problem

--- src/Kerbalism/UI/Icons.cs
+++ src/Kerbalism/UI/Icons.cs
@@ -103,100 +103,116 @@ namespace KERBALISM
 		internal static float nextFlashing = Time.time;
 		internal static bool lastIcon = false;
 
+		internal static Texture2D GetTexture(string name) {
+#if KSP13
+			return Lib.GetTexture(name);
+#else
+			return Styles.GetUIScaledTexture(name);
+#endif
+		}
+
+		internal static Texture2D GetTexture(string name, int width = 16, int height = 16, float prescalar = 1.0f) {
+#if KSP13
+			return Lib.GetTexture(name);
+#else
+			return Styles.GetUIScaledTexture(name, width, height, prescalar);
+#endif
+		}
+
 		///<summary> Initializes the icons </summary>
 		internal static void Initialize()
 		{
 			TexturePath = KSPUtil.ApplicationRootPath + "GameData/Kerbalism/Textures/";
 
-			empty = Styles.GetUIScaledTexture("empty");                      // an empty icon to maintain alignment
-			close = Styles.GetUIScaledTexture("close");                      // black close icon
-			left_arrow = Styles.GetUIScaledTexture("left-arrow");            // white left arrow
-			right_arrow = Styles.GetUIScaledTexture("right-arrow");          // white right arrow
-			toggle_green = Styles.GetUIScaledTexture("toggle-green");        // green check mark
-			toggle_red = Styles.GetUIScaledTexture("toggle-red");            // red check mark
+			empty = GetTexture("empty");                      // an empty icon to maintain alignment
+			close = GetTexture("close");                      // black close icon
+			left_arrow = GetTexture("left-arrow");            // white left arrow
+			right_arrow = GetTexture("right-arrow");          // white right arrow
+			toggle_green = GetTexture("toggle-green");        // green check mark
+			toggle_red = GetTexture("toggle-red");            // red check mark
 
-			send_black = Styles.GetUIScaledTexture("send-black");            // used by file man
-			send_cyan = Styles.GetUIScaledTexture("send-cyan");
-			lab_black = Styles.GetUIScaledTexture("lab-black");
-			lab_cyan = Styles.GetUIScaledTexture("lab-cyan");
+			send_black = GetTexture("send-black");            // used by file man
+			send_cyan = GetTexture("send-cyan");
+			lab_black = GetTexture("lab-black");
+			lab_cyan = GetTexture("lab-cyan");
 
-			applauncher = Styles.GetUIScaledTexture("applauncher", 38, 38);
+			applauncher = GetTexture("applauncher", 38, 38);
 
-			small_info = Styles.GetUIScaledTexture("small-info");
-			small_folder = Styles.GetUIScaledTexture("small-folder");
-			small_console = Styles.GetUIScaledTexture("small-console");
-			small_config = Styles.GetUIScaledTexture("small-config");
-			small_search = Styles.GetUIScaledTexture("small-search");
-			small_notes = Styles.GetUIScaledTexture("small-notes");
+			small_info = GetTexture("small-info");
+			small_folder = GetTexture("small-folder");
+			small_console = GetTexture("small-console");
+			small_config = GetTexture("small-config");
+			small_search = GetTexture("small-search");
+			small_notes = GetTexture("small-notes");
 
-			category_normal = Styles.GetUIScaledTexture("category-normal");
-			category_selected = Styles.GetUIScaledTexture("category-selected");
+			category_normal = GetTexture("category-normal");
+			category_selected = GetTexture("category-selected");
 
-			sun_black = Styles.GetUIScaledTexture("sun-black");
-			sun_white = Styles.GetUIScaledTexture("sun-white");
+			sun_black = GetTexture("sun-black");
+			sun_white = GetTexture("sun-white");
 
-			battery_white = Styles.GetUIScaledTexture("battery-white");
-			battery_yellow = Styles.GetUIScaledTexture("battery-yellow");
-			battery_red = Styles.GetUIScaledTexture("battery-red");
+			battery_white = GetTexture("battery-white");
+			battery_yellow = GetTexture("battery-yellow");
+			battery_red = GetTexture("battery-red");
 
-			box_white = Styles.GetUIScaledTexture("box-white");
-			box_yellow = Styles.GetUIScaledTexture("box-yellow");
-			box_red = Styles.GetUIScaledTexture("box-red");
+			box_white = GetTexture("box-white");
+			box_yellow = GetTexture("box-yellow");
+			box_red = GetTexture("box-red");
 
-			wrench_white = Styles.GetUIScaledTexture("wrench-white");
-			wrench_yellow = Styles.GetUIScaledTexture("wrench-yellow");
-			wrench_red = Styles.GetUIScaledTexture("wrench-red");
+			wrench_white = GetTexture("wrench-white");
+			wrench_yellow = GetTexture("wrench-yellow");
+			wrench_red = GetTexture("wrench-red");
 
-			signal_white = Styles.GetUIScaledTexture("signal-white");
-			signal_yellow = Styles.GetUIScaledTexture("signal-yellow");
-			signal_red = Styles.GetUIScaledTexture("signal-red");
+			signal_white = GetTexture("signal-white");
+			signal_yellow = GetTexture("signal-yellow");
+			signal_red = GetTexture("signal-red");
 
-			recycle_yellow = Styles.GetUIScaledTexture("recycle-yellow");
-			recycle_red = Styles.GetUIScaledTexture("recycle-red");
+			recycle_yellow = GetTexture("recycle-yellow");
+			recycle_red = GetTexture("recycle-red");
 
-			radiation_yellow = Styles.GetUIScaledTexture("radiation-yellow");
-			radiation_red = Styles.GetUIScaledTexture("radiation-red");
+			radiation_yellow = GetTexture("radiation-yellow");
+			radiation_red = GetTexture("radiation-red");
 
-			health_white = Styles.GetUIScaledTexture("health-white");
-			health_yellow = Styles.GetUIScaledTexture("health-yellow");
-			health_red = Styles.GetUIScaledTexture("health-red");
+			health_white = GetTexture("health-white");
+			health_yellow = GetTexture("health-yellow");
+			health_red = GetTexture("health-red");
 
-			brain_white = Styles.GetUIScaledTexture("brain-white");
-			brain_yellow = Styles.GetUIScaledTexture("brain-yellow");
-			brain_red = Styles.GetUIScaledTexture("brain-red");
+			brain_white = GetTexture("brain-white");
+			brain_yellow = GetTexture("brain-yellow");
+			brain_red = GetTexture("brain-red");
 
-			storm_yellow = Styles.GetUIScaledTexture("storm-yellow");
-			storm_red = Styles.GetUIScaledTexture("storm-red");
+			storm_yellow = GetTexture("storm-yellow");
+			storm_red = GetTexture("storm-red");
 
-			plant_white = Styles.GetUIScaledTexture("plant-white");
-			plant_yellow = Styles.GetUIScaledTexture("plant-yellow");
+			plant_white = GetTexture("plant-white");
+			plant_yellow = GetTexture("plant-yellow");
 
-			station_black = Styles.GetUIScaledTexture("vessels/station-black", 80, 80, 4);
-			station_white = Styles.GetUIScaledTexture("vessels/station-white", 80, 80, 4);
+			station_black = GetTexture("vessels/station-black", 80, 80, 4);
+			station_white = GetTexture("vessels/station-white", 80, 80, 4);
 
-			base_black = Styles.GetUIScaledTexture("vessels/base-black", 80, 80, 4);
-			base_white = Styles.GetUIScaledTexture("vessels/base-white", 80, 80, 4);
+			base_black = GetTexture("vessels/base-black", 80, 80, 4);
+			base_white = GetTexture("vessels/base-white", 80, 80, 4);
 
-			ship_black = Styles.GetUIScaledTexture("vessels/ship-black", 80, 80, 4);
-			ship_white = Styles.GetUIScaledTexture("vessels/ship-white", 80, 80, 4);
+			ship_black = GetTexture("vessels/ship-black", 80, 80, 4);
+			ship_white = GetTexture("vessels/ship-white", 80, 80, 4);
 
-			probe_black = Styles.GetUIScaledTexture("vessels/probe-black", 80, 80, 4);
-			probe_white = Styles.GetUIScaledTexture("vessels/probe-white", 80, 80, 4);
+			probe_black = GetTexture("vessels/probe-black", 80, 80, 4);
+			probe_white = GetTexture("vessels/probe-white", 80, 80, 4);
 
-			relay_black = Styles.GetUIScaledTexture("vessels/relay-black", 40, 40, 1.75f);
-			relay_white = Styles.GetUIScaledTexture("vessels/relay-white", 40, 40, 1.75f);
+			relay_black = GetTexture("vessels/relay-black", 40, 40, 1.75f);
+			relay_white = GetTexture("vessels/relay-white", 40, 40, 1.75f);
 
-			rover_black = Styles.GetUIScaledTexture("vessels/rover-black", 80, 80, 4);
-			rover_white = Styles.GetUIScaledTexture("vessels/rover-white", 80, 80, 4);
+			rover_black = GetTexture("vessels/rover-black", 80, 80, 4);
+			rover_white = GetTexture("vessels/rover-white", 80, 80, 4);
 
-			lander_black = Styles.GetUIScaledTexture("vessels/lander-black", 80, 80, 4);
-			lander_white = Styles.GetUIScaledTexture("vessels/lander-white", 80, 80, 4);
+			lander_black = GetTexture("vessels/lander-black", 80, 80, 4);
+			lander_white = GetTexture("vessels/lander-white", 80, 80, 4);
 
-			eva_black = Styles.GetUIScaledTexture("vessels/eva-black", 80, 80, 4);
-			eva_white = Styles.GetUIScaledTexture("vessels/eva-white", 80, 80, 4);
+			eva_black = GetTexture("vessels/eva-black", 80, 80, 4);
+			eva_white = GetTexture("vessels/eva-white", 80, 80, 4);
 
-			plane_black = Styles.GetUIScaledTexture("vessels/plane-black", 40, 40, 2.25f);
-			plane_white = Styles.GetUIScaledTexture("vessels/plane-white", 40, 40, 2.25f);
+			plane_black = GetTexture("vessels/plane-black", 40, 40, 2.25f);
+			plane_white = GetTexture("vessels/plane-white", 40, 40, 2.25f);
 	}
 
 		/// <summary>Switch icons based on time </summary>
