3a4c9784c0e238e5e8e95c1ae888e8a1360bafe0
Martin Domig <martin@domig.net>
2019-03-31 11:10:31 +0200

    antenna power nerf when science is enabled

--- src/Kerbalism/Comms/AntennaInfoCommNet.cs
+++ src/Kerbalism/Comms/AntennaInfoCommNet.cs
@@ -30,6 +30,8 @@ namespace KERBALISM
 						t.Events["StopTransmission"].active = false;
 						t.Actions["StartTransmissionAction"].active = false;
 
+						Lib.Log("Data rate: " + t.name + " " + t.DataRate);
+
 						if (t.antennaType == AntennaType.INTERNAL) // do not include internal data rate, ec cost only
 							ec += t.DataResourceCost * t.DataRate;
 						else
@@ -107,6 +109,13 @@ namespace KERBALISM
 						}
 					}
 				}
+			}
+
+			if(Settings.Science)
+			{
+				// Rate right now is in Mb/s, which is OP.
+				// Let's assume it is in kB/s instead
+				rate /= 8 * 1024.0;
 			}
 		}
 	}
