975f6688b3c3ae93a760489317bb86dca2f46447
Martin Domig <martin@domig.net>
2019-04-11 23:13:40 +0200

    improved text that appears on transfer

--- src/Kerbalism/Science/Drive.cs
+++ src/Kerbalism/Science/Drive.cs
@@ -353,7 +353,7 @@ namespace KERBALISM
 			if (allMoved)
 				Message.Post
 				(
-					Localizer.Format("#KERBALISM_Science_ofdatatransfer"),
+					Lib.HumanReadableDataSize(dataAmount) + Localizer.Format("#KERBALISM_Science_ofdatatransfer"),
 				 	Lib.BuildString(Localizer.Format("#KERBALISM_Generic_FROM"), " <b>", src.vesselName, "</b> ", Localizer.Format("#KERBALISM_Generic_TO"), " <b>", dst.vesselName, "</b>")
 				);
 			else
