7603265bd9d447c77d02a4f208e682f8a95afe3a
Martin Domig <martin@domig.net>
2019-05-17 22:59:28 +0200

    debug setup for mac

--- buildscripts/setup_mac.sh
+++ buildscripts/setup_mac.sh
@@ -13,11 +13,14 @@ echo "export KSPDEVDIR=$MY_KSP_DIR" >> ~/.bash_profile
 . ~/.bash_profile
 
 cd $KSPDEVDIR
+ln -s $UNITY_DIR/Unity.app/Contents/PlaybackEngines/MacStandaloneSupport/Variations/macosx64_development_mono/UnityPlayer.app KSP_Debug.app
 rm -rf KSP_x64_Data
 mkdir -p KSP_x64_Data/Managed
+ln -s KSP_x64_Data KSP_x64_Dbg_Data
 cd KSP_x64_Data/Managed
 
 ln -s $UNITY_DIR/Unity.app/Contents/Managed/* .
 ln -s ../../KSP.app/Contents/Resources/Data/Managed/* . 2>/dev/null
 
 cd $CURRENT_DIR
+cp PlayerConnectionConfigFile.dms $KSPDEVDIR/KSP_x64_Data
