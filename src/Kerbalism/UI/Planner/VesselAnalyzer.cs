10afad20b0aa2ea8258e79cb89df279baf9ba0e7
sswelm <sswelm@gmail.com>
2019-07-01 07:01:54 +0200

    Calculate shielding with corect exponential function
    If you have a thickness which stops a known amount of radiation of a known and constant type, then if you have a new thickness, you can calculate how much it stops by:
    
    Stoppage = 1 - ((1 - AmountStoppedByKnownThickness)^(NewThickness / KnownThickness))
    
    source : http://www.projectrho.com/public_html/rocket/radiation.php
    
    modified formula will give same result when no result shielding or max shielding

--- src/Kerbalism/UI/Planner/VesselAnalyzer.cs
+++ src/Kerbalism/UI/Planner/VesselAnalyzer.cs
@@ -167,7 +167,10 @@ namespace KERBALISM.Planner
 			// calculate shielding factor
 			double amount = sim.Resource("Shielding").amount;
 			double capacity = sim.Resource("Shielding").capacity;
-			shielding = (capacity > double.Epsilon ? amount / capacity : 1.0) * PreferencesStorm.Instance.shieldingEfficiency;
+			
+			shielding = capacity > double.Epsilon 
+                ? 1 - Math.Pow(1 - PreferencesStorm.Instance.shieldingEfficiency, amount / capacity) 
+                : PreferencesStorm.Instance.shieldingEfficiency;
 		}
 
 		void Analyze_reliability(List<Part> parts)
