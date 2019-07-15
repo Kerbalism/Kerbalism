416fc10dc2672f83989f4e82c2b4bb08c3a713ae
SirMortimer <his.mortimerness@gmail.com>
2019-07-15 09:06:44 +0200

    removed Lib.Log lines that were meant for debugging

--- src/Kerbalism/Patches/DeployedScienceExperiment_SendDataToComms.cs
+++ src/Kerbalism/Patches/DeployedScienceExperiment_SendDataToComms.cs
@@ -17,7 +17,7 @@ namespace KERBALISM
 			float storedScienceData = Lib.ReflectionValue<float>(__instance, "storedScienceData");
 			float transmittedScienceData = Lib.ReflectionValue<float>(__instance, "transmittedScienceData");
 			Vessel ControllerVessel = Lib.ReflectionValue<Vessel>(__instance, "ControllerVessel");
-			Lib.Log("SendDataToComms!: " + subject.title);
+			//Lib.Log("SendDataToComms!: " + subject.title);
 			if (__instance.Experiment != null && !(__instance.ExperimentVessel == null) && subject != null && !(__instance.Cluster == null) && __instance.sciencePart.Enabled && !(storedScienceData <= 0f) && __instance.ExperimentSituationValid) {
 				if (!__instance.TimeToSendStoredData())
 				{
@@ -32,20 +32,20 @@ namespace KERBALISM
 				Part control;
 				FlightGlobals.FindLoadedPart(__instance.Cluster.ControlModulePartId, out control);
 				if(control == null) {
-					Lib.Log("DeployedScienceExperiment: couldn't find control module");
+					//Lib.Log("DeployedScienceExperiment: couldn't find control module");
 					__result = true;
 					return false;
 				}
 				List<Drive> drives = Drive.GetDrives(control.vessel, false);
 				foreach (Drive drive in drives) {
-					Lib.Log(Lib.BuildString("BREAKING GROUND -- ", subject.id, " | ", storedScienceData.ToString()));
+					//Lib.Log(Lib.BuildString("BREAKING GROUND -- ", subject.id, " | ", storedScienceData.ToString()));
 					if(drive.Record_file(subject.id, storedScienceData, true)) {
-						Lib.Log("BREAKING GROUND -- file recorded!");
+						//Lib.Log("BREAKING GROUND -- file recorded!");
 						Lib.ReflectionValue<float>(__instance, "transmittedScienceData", transmittedScienceData + storedScienceData);
 						Lib.ReflectionValue<float>(__instance, "storedScienceData", 0f);
 						break;
 					} else {
-						Lib.Log("BREAKING GROUND -- file NOT recorded!");
+						//Lib.Log("BREAKING GROUND -- file NOT recorded!");
 						__result = true;
 						return false;
 					}
