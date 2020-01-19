Kerbalism/Kerbalism

commit bd7b2641d3b5a96abad59409d863f672e593a303

initialize storm warning = true for vessels with crew on board

src/Kerbalism/Database/VesselData.cs
@@ -583,6 +583,7 @@ namespace KERBALISM
 
 
 			FieldsDefaultInit();
+			cfg_storm |= Features.SpaceWeather && Lib.CrewCount(vessel) > 0;
 
 			Lib.LogDebug("VesselData ctor (new vessel) : id '" + VesselId + "' (" + Vessel.vesselName + "), part count : " + parts.Count);
 			UnityEngine.Profiling.Profiler.EndSample();
@@ -608,6 +609,7 @@ namespace KERBALISM
 			if (node == null)
 			{
 				FieldsDefaultInit();
+				cfg_storm |= Features.SpaceWeather && Lib.CrewCount(protoVessel.vesselRef) > 0;
 				Lib.LogDebug("VesselData ctor (created from protovessel) : id '" + VesselId + "' (" + protoVessel.vesselName + "), part count : " + parts.Count);
 			}
 			else

commit ea29f97d8c1031508f32817da2cb2aa8f01e5220

Fix VesselData ctor crash introduced in commit bd7b264 (storm cfg init with a null vessel)

src/Kerbalism/Database/VesselData.cs
@@ -635,8 +635,7 @@ namespace KERBALISM
 					parts.Add(protopart.flightID, new PartData(protopart));
 
 
-			FieldsDefaultInit();
-			cfg_storm |= Features.SpaceWeather && Lib.CrewCount(vessel) > 0;
+			FieldsDefaultInit(vessel.protoVessel);
 
 			Lib.LogDebug("VesselData ctor (new vessel) : id '" + VesselId + "' (" + Vessel.vesselName + "), part count : " + parts.Count);
 			UnityEngine.Profiling.Profiler.EndSample();
@@ -661,8 +660,7 @@ namespace KERBALISM
 
 			if (node == null)
 			{
-				FieldsDefaultInit();
-				cfg_storm |= Features.SpaceWeather && Lib.CrewCount(protoVessel.vesselRef) > 0;
+				FieldsDefaultInit(protoVessel);
 				Lib.LogDebug("VesselData ctor (created from protovessel) : id '" + VesselId + "' (" + protoVessel.vesselName + "), part count : " + parts.Count);
 			}
 			else
@@ -673,7 +671,7 @@ namespace KERBALISM
 			UnityEngine.Profiling.Profiler.EndSample();
 		}
 
-		private void FieldsDefaultInit()
+		private void FieldsDefaultInit(ProtoVessel pv)
 		{
 			msg_signal = false;
 			msg_belt = false;
@@ -681,7 +679,7 @@ namespace KERBALISM
 			cfg_supply = PreferencesMessages.Instance.supply;
 			cfg_signal = PreferencesMessages.Instance.signal;
 			cfg_malfunction = PreferencesMessages.Instance.malfunction;
-			cfg_storm = PreferencesMessages.Instance.storm;
+			cfg_storm = Features.SpaceWeather && PreferencesMessages.Instance.storm && Lib.CrewCount(pv) > 0;
 			cfg_script = PreferencesMessages.Instance.script;
 			cfg_highlights = PreferencesReliability.Instance.highlights;
 			cfg_showlink = true;

src/Kerbalism/Lib.cs
@@ -1210,17 +1210,22 @@ namespace KERBALISM
 		}
 
 
-		///<summary>return set of crew on a vessel</summary>
+		///<summary>return set of crew on a vessel. Works on loaded and unloaded vessels</summary>
 		public static List<ProtoCrewMember> CrewList(Vessel v)
 		{
 			return v.loaded ? v.GetVesselCrew() : v.protoVessel.GetVesselCrew();
 		}
 
-
-		///<summary>return crew count of a vessel</summary>
+		///<summary>return crew count of a vessel. Works on loaded and unloaded vessels</summary>
 		public static int CrewCount(Vessel v)
 		{
 			return v.isEVA ? 1 : CrewList(v).Count;
+		}
+
+		///<summary>return crew count of a protovessel</summary>
+		public static int CrewCount(ProtoVessel pv)
+		{
+			return pv.vesselType == VesselType.EVA ? 1 : pv.GetVesselCrew().Count();
 		}
 
 		///<summary>return crew capacity of a vessel</summary>