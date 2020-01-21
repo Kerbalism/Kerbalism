829de0d36273dfd363cfd3a4aa1e36ba0235eddf
Gotmachine <24925209+gotmachine@users.noreply.github.com>
2020-01-22 00:53:52 +0100

    Fix compilation errors on .NET 3.5

--- src/Kerbalism/LocalizationCache.cs
+++ src/Kerbalism/LocalizationCache.cs
@@ -1,9 +1,4 @@
-using System;
-using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using KSP.Localization;
 using System.Text.RegularExpressions;
 
@@ -63,7 +58,7 @@ namespace KERBALISM
 					continue;
 				}
 			}
-			System.IO.File.WriteAllLines(@"C:\Users\Got\Desktop\loctest\loctemplate.txt", locFile);
+			System.IO.File.WriteAllLines(@"C:\Users\Got\Desktop\loctest\loctemplate.txt", locFile.ToArray());
 		}
 
 		////////////////////////////////////////////////////////////////////
