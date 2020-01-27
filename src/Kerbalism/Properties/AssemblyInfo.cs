63284279ac3582507db9e13f4fc7a91f80ca071a
SirMortimer <his.mortimerness@gmail.com>
2020-01-27 09:41:18 +0100

    Version 3.3 update finished

--- src/Kerbalism/Properties/AssemblyInfo.cs
+++ src/Kerbalism/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-#region Using directives
+﻿#region Using directives
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("3.2")]
-[assembly: AssemblyFileVersion("3.2")]
+[assembly: AssemblyVersion("3.3")]
+[assembly: AssemblyFileVersion("3.3")]
