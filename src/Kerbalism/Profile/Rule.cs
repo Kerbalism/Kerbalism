871e0ed3f9483246d7bc6788469a4507c72d06f9
siimav <1120038+siimav@users.noreply.github.com>
2026-08-19 15:27:16 +0300

    Use vessel temperature when calculating climatization power (#1183)
    Continuation of #1017.
    In most cases this should result in significantly reduced climatization
    power needs.
    A hab I have landed on the Moon dropped from 623W to 239W during
    daytime.

--- src/Kerbalism/Profile/Rule.cs
+++ src/Kerbalism/Profile/Rule.cs
@@ -70,7 +70,7 @@ namespace KERBALISM
 			ResourceInfo res = input.Length > 0 ? resources.GetResource(v, input) : null;
 
 			// determine message variant
-			uint variant = vd.EnvTemperature < Settings.LifeSupportSurvivalTemperature ? 0 : 1u;
+			uint variant = vd.VesselTemperature < Settings.LifeSupportSurvivalTemperature ? 0 : 1u;
 
 			// get product of all environment modifiers
 			double k = Modifiers.Evaluate(v, vd, resources, modifiers);
